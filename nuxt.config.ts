23e37a60a342881ce83b51678b6d477d0085c089
A.Rahman Al-Khateeb <khateeboveskey@gmail.com>
2025-05-26 19:17:47 +0300

    build: add nuxt-ui-pro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,4 +11,5 @@ export default defineNuxtConfig({
       tailwindcss(),
     ],
   },
+  modules: ['@nuxt/ui-pro']
 })
--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-screen w-screen flex items-center justify-center">
-    Hi
-  </div>
+  <UApp>
+    <NuxtPage />
+  </UApp>
 </template>
