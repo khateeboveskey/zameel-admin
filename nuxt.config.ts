a2b2da0074b6ff3f124836a62049b2ebcc58a09b
A.Rahman Al-Khateeb <khateeboveskey@gmail.com>
2025-05-27 09:49:59 +0300

    revert: update srcDir

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  srcDir: 'src/',
+  srcDir: 'app/',
   vite: {
     plugins: [
       tailwindcss(),
