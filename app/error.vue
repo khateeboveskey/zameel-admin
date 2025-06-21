1772fa5a46a152db39fd24bd7a08aa6d320cbc2a
A.Rahman Al-Khateeb <khateeboveskey@gmail.com>
2025-06-21 20:01:06 +0300

    feat: add error handling component with dynamic error display

---
+++ app/error.vue
@@ -0,0 +1,32 @@
+<template>
+  <NuxtLayout>
+    <NuxtLoadingIndicator color="var(--color-indigo-500)" />
+    <UApp :locale="ar">
+      <div class="h-screen flex items-center justify-center gap-5 flex-col">
+        <h2 class="text-9xl font-black text-indigo-500">
+          {{ error?.statusCode }}
+        </h2>
+        <ErrorLogo class="w-1/4 my-15" />
+        <p class="text-left text-gray-500 -mt-5" dir="ltr">
+          {{ error }}
+        </p>
+        <UButton icon="i-lucide-home" to="/"
+          >العودة إلى الصفحة الرئيسية</UButton
+        >
+      </div>
+    </UApp>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+import { ar } from '@nuxt/ui-pro/locale';
+import type { NuxtError } from '#app';
+
+const props = defineProps({
+  error: Object as () => NuxtError,
+});
+
+useHead({
+  title: 'خطأ ' + props.error?.message,
+});
+</script>
